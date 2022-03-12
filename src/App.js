73f59dbacbd609f5a8f149f31ed4612b9477c49e
Fang-Wen Lee <66965736+fang-wenlee@users.noreply.github.com>
2022-03-12 12:41:56 -0800

    mskeStyle fun const useStyles = makeStyles({})

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./styles.css";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles({
+  buttonStyle: { color: "red" },
+  textStyle: { color: "green" }
+});
 
 export default function App() {
+  const classes = useStyles();
+  console.log(classes);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Apply material UI to React Component</h1>
+      <Button className={classes.buttonStyle}>Click Me</Button>
+      <Button className={classes.textStyle}>Submit file</Button>
     </div>
   );
 }
