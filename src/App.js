425e6649b05d6eceb3bc2e2a2f4b05f03ccc5885
Fang-Wen Lee <66965736+fang-wenlee@users.noreply.github.com>
2022-03-12 15:04:23 -0800

    use Theme and props use Theme and props with material uI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from "react";
 import CoolButton from "./CoolButton";
 
 export default function App() {
-  const cool = true;
-  return <CoolButton cool={cool} />;
+  // const cool = true;
+  return <CoolButton cool={true} />;
 }
--- src/CoolButton.js
+++ src/CoolButton.js
@@ -2,14 +2,37 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  buttonStyle: { color: (props) => (props.cool ? "blue" : "yellow") }
-});
+// const useStyles = makeStyles({
+//   buttonStyle: { color: (props) => (props.cool ? "blue" : "yellow") }
+// });
+
+// const useStyles = makeStyles({
+//   buttonStyle: (props) => {
+//     return {
+//       color: props.cool ? "red" : "yellow",
+//       backgroundColor: props.cool ? "orange" : "green"
+//     };
+//   }
+// });
+//apply theem
+const useStyles = makeStyles((theme) => ({
+  buttonStyle: (props) => {
+    return {
+      color: props.cool ? "red" : "yellow",
+      [theme.breakpoints.up("sm")]: { color: "cyan" },
+      backgroundColor: props.cool ? "orange" : "green"
+    };
+  }
+}));
 
 const CoolButton = (props) => {
   const classes = useStyles(props);
 
-  return <Button className={classes.buttonStyle}>Cool button</Button>;
+  return (
+    <Button fullWidth className={classes.buttonStyle}>
+      Cool button
+    </Button>
+  );
 };
 
 export default CoolButton;
