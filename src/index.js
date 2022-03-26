49589e8c4d3082dadbad53e4bc3d23e4e9c07056
Fang-Wen Lee <66965736+fang-wenlee@users.noreply.github.com>
2022-03-26 09:13:28 -0700

    wrap around App comp with ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
   rootElement
 );
